a1cd6cc71b25a42fbe32735b586c6e553276b620
SharifMinhazul <sm.emon002@gmail.com>
2020-12-10 16:26:56 +0600

    changed template

--- main.cc
+++ main.cc
@@ -1,24 +1,32 @@
-// Create your own template by modifying this file!
+/*
+* Author  : Sharif Minhazul Islam Emon
+* Created : 
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef DEBUG
-     #define debug(args...)            {dbg,args; cerr<<endl;}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << "  " << H; dbg_out(T...); }
+
+#ifdef SMIE
+    #define debug(args...) cerr << "(" << #args << "):", dbg_out(args)
 #else
-    #define debug(args...)              // Just strip off all debug tokens
+    #define debug(args...)
 #endif
 
-struct debugger
-{
-    template<typename T> debugger& operator , (const T& v)
-    {    
-        cerr<<v<<" ";    
-        return *this;    
+int main() {
+    int tests;
+    cin >> tests;
+
+    while (tests-- > 0) {
+        vector<pair<int, int>> a;
+
+        a.emplace_back(2, 3);
+
+        debug(a);
     }
-} dbg;
-
-int main() 
-{
-
 }
-
